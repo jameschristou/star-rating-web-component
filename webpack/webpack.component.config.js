817829c0c8b094bb8c4e956805a4d47c7f6ae476
jameschristou <james.christou@gmail.com>
2020-05-22 17:43:53 +1000

    Setting up for publishing to npm public

---
+++ webpack/webpack.component.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    ReactAutoCompleteTagsInput: './src/StarRating.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '../', 'dist-component'),
+    publicPath: '/',
+    filename: 'StarRating.js',
+    libraryTarget: 'umd',
+    library: 'star-rating-web-component',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: 'StarRating.css' })
+  ]
+};
